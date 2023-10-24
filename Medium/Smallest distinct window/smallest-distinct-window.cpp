e31f6246581bd2f556d6a4319e01c8a18f7933de
Faheemuddin Naseem <fnaseem010@gmail.com>
2023-10-24 12:05:29 +0530

    Updated Solution - GfG to GitHub

--- Medium/Smallest distinct window/smallest-distinct-window.cpp
+++ Medium/Smallest distinct window/smallest-distinct-window.cpp
@@ -8,26 +8,26 @@ class Solution{
     public:
     int findSubString(string str)
     {
-        // Your code goes here 
-        unordered_map<char,int>map,map1;
-        for(auto it:str)map[it]++;
-        int i=0,j=0,ans=INT_MAX;
-        
-        while(j<str.size())
-        {
-            map1[str[j]]++;
-            
-            while(map1.size()==map.size())
-            {
-                ans=min(ans,j-i+1);
-                map1[str[i]]--;
-                if(map1[str[i]]==0)map1.erase(str[i]);
+        // Your code goes here  
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        int ans=INT_MAX;
+        for(auto it:str){
+            mp1[it]++;
+        }
+        int i=0,j=0;
+        while(j<str.size()){
+            // string temp=str[j];
+            mp2[str[j]]++;
+            while(mp2.size()==mp1.size()){
+                ans=min(ans,(j-i+1));
+                mp2[str[i]]--;
+                if(mp2[str[i]]==0)mp2.erase(str[i]);
                 i++;
             }
             j++;
         }
         return ans;
-        
     }
 };
 
