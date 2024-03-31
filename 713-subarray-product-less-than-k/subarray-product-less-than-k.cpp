2a7686d2ee4e350bad16ecb28da2aea4f82c443d
Faheemuddin Naseem <fnaseem010@gmail.com>
2024-03-31 07:00:20 +0530

    Time: 56 ms (60.80%) | Memory: 63.7 MB (56.23%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-         if (k == 0)
-            return 0;
-        int n = nums.size(), product = 1, cnt {};
-        int l = 0, r = 0;
-        while(r < n)
-        {
-            product *= nums[r++];
-            while(l < r && product >= k)
-                product /= nums[l++];
-            
-            cnt += (r - l);
+        if(k==0)return 0;
+        int l=0,r=0,cnt=0,prod=1;
+        int n=nums.size();
+        while(r<n){
+            prod*=nums[r];
+            r++;
+            while(l<r && prod>=k){
+                prod/=nums[l];
+                l++;
+            }
+            cnt+=(r-l);
         }
         return cnt;
     }
