67cb0bb80616937d3e6b77698577da03abe71b47
Faheemuddin Naseem <fnaseem010@gmail.com>
2024-10-07 00:35:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
+++ Difficulty: Easy/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -19,16 +19,22 @@ struct Node {
 //Function to find the lowest common ancestor in a BST.
 class Solution{
     public:
+      
         Node* LCA(Node *root, int n1, int n2)
         {
             // code here
-            if(!root)return root;
-            if(root->data==n1 || root->data==n2)return root;
-            Node* leftans=LCA(root->left,n1,n2);
-            Node* rightans=LCA(root->right,n1,n2);
-            if(leftans && rightans)return root;
-            else if(leftans)return leftans;
-            else return rightans;
+              if(root==NULL)
+        {
+            return root;
+        }
+       
+        if(root->data>n1 && root->data>n2)
+         return LCA(root->left,n1,n2);
+        
+        if(root->data<n1 && root->data<n2 )
+         return LCA(root->right,n1,n2);
+        
+        return root;
         }
 
 };
